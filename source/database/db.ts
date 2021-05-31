00fc57c0899e91651a1ba6ec89e8d55c4d08c29f
Godinson1 <josephgodwin274@yahoo.com>
2021-05-31 19:17:47 +0100

    setup database

--- source/database/db.ts
+++ source/database/db.ts
@@ -7,7 +7,9 @@ const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPAS
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
